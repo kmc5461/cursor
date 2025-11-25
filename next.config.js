14596d67723bd579d9cb5194e90f954844164511
kmc5461 <kamaciacente@gmail.com>
2025-11-25 09:19:17 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+// next.config.js
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+});
 
-module.exports = nextConfig;
+module.exports = withPWA({
+  reactStrictMode: true,
+  experimental: {
+    optimizeCss: true,
+    optimizePackageImports: ["framer-motion"],
+  },
+  images: {
+    unoptimized: true,
+  },
+  output: "standalone",
+});
